25f6349ca9a18d00125048c42b064cbb31b0a10e
rmendes1 <joaorenatomendes@gmail.com>
2022-06-18 15:03:49 -0300

    (Feature: TD-1) Add login functionality
    Logout url added to path.

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from accounts.views import login_view
+from accounts.views import (
+    login_view,
+    logout_view)
 from .views import home_view
 from articles.views import (
     article_create_view,
@@ -31,4 +33,5 @@ urlpatterns = [
     path('articles/<int:id>/', article_detail_view),
     path('admin/', admin.site.urls),
     path('login/', login_view),
+    path('logout/', logout_view),
 ]
