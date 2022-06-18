a9c70bb42ae649e6d9b5c78f09712470e0614f57
rmendes1 <joaorenatomendes@gmail.com>
2022-06-18 14:43:06 -0300

    (Feature: TD-1) Add login functionality
    login view URL added to path

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -15,13 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from accounts.views import login_view
 from .views import home_view
-from articles import views
+from articles.views import (
+    article_create_view,
+    article_detail_view,
+    article_search_view
+    )
+
 
 urlpatterns = [
-    path('', home_view), # index / home / root
-    path('articles/', views.article_search_view),
-    path('articles/create/', views.article_create_view),
-    path('articles/<int:id>/', views.article_detail_view),
+    path('', home_view),  # index / home / root
+    path('articles/', article_search_view),
+    path('articles/create/', article_create_view),
+    path('articles/<int:id>/', article_detail_view),
     path('admin/', admin.site.urls),
+    path('login/', login_view),
 ]
