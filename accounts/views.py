c8ba84c4114b3f6bd598e045d5b6875422856fd2
rmendes1 <joaorenatomendes@gmail.com>
2022-06-18 15:02:31 -0300

    (Feature: TD-1) Add login functionality
    Logout view added.

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 
 def login_view(request):
@@ -13,3 +13,10 @@ def login_view(request):
         login(request, user)
         return redirect('/')
     return render(request, "accounts/login.html", context={})
+
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("/login/")
+    return render(request, "accounts/logout.html", {})
