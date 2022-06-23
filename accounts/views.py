a192926646523eb8294701909384e57f5f56bc21
rmendes1 <joaorenatomendes@gmail.com>
2022-06-23 16:24:17 -0300

    (Refactor: TD-4) Add authentication to login
    login_view is now using authenticationform

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
 def register_view(request):
@@ -13,16 +13,20 @@ def register_view(request):
 
 
 def login_view(request):
+    # future -> ?next=/articles/create
     if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(request, username=username, password=password)
-        if user is None:
-            context = {"error": "Invalid username or password"}
-            return render(request, "accounts/login.html", context)
-        login(request, user)
-        return redirect('/')
-    return render(request, "accounts/login.html", context={})
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+
+    else:
+        form = AuthenticationForm(request)
+    context = {
+        "form": form
+    }
+    return render(request, "accounts/login.html", context=context)
 
 
 def logout_view(request):
