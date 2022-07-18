d858de262fa6808096426e37a231c78cdc3e0f5a
joaormendes <joaorenatomendes@fontedeprecos.com.br>
2022-07-18 20:58:55 -0300

    (Refactor: TD-6) Articles model
    Timestamp, updated and publish added to admin

--- articles/models.py
+++ articles/models.py
@@ -1,11 +1,23 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Article(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField(blank=True, null=True)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     publish = models.DateField(auto_now_add=False,
                                null=True,
                                blank=True)
+
+    # Overriding save method
+    def save(self, *args, **kwargs):
+        # obj = Article.objects.get(id=1)
+        # set something
+        if self.slug is None:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+        # obj.save()
+        # do another something
