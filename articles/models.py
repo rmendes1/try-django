b96db62baa1c9695e7c890a74bd7c5c6c171c756
joaormendes <joaorenatomendes@fontedeprecos.com.br>
2022-07-18 20:36:02 -0300

    (Refactor: TD-6) Articles model
    - Title changed to charfield
    - Timestamp, updated, and publish have been created.

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
 class Article(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=120)
     content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    publish = models.DateField(auto_now_add=False,
+                               null=True,
+                               blank=True)
