495e56b60b946a986c24125912971e29f760a254
joaormendes <joaorenatomendes@fontedeprecos.com.br>
2022-08-09 15:39:07 -0300

    (Feature: TD-16) Admin inlines for FK
    Inlines have been added to users admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import RecipeIngredient, Recipe
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+
+User = get_user_model()
+
+
+class RecipeIngredientInline(admin.StackedInline):
+    model = RecipeIngredient
+    extra = 0
+    # fields = ['name', 'quantity', 'unit', 'directions']
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInline]
+    list_display = ['name', 'user']
+    readonly_fields = ['timestamp', 'updated']
+    raw_id_fields = ['user']  # makes you search for the field in a separate search window
+
+
+admin.site.register(Recipe, RecipeAdmin)
